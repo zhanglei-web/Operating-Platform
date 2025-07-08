cbdcdf285930b4f740fe6fc9696b480b3425cf89
Xiang Yang <ryu-yang@qq.com>
2025-07-08 16:12:48 +0800

    update docker start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -21,9 +21,8 @@ docker run -it \
   -e XDG_RUNTIME_DIR=/tmp \
   -e http_proxy=http://127.0.0.1:7897 \
   -e https_proxy=http://127.0.0.1:7897 \
-  -e HF_HOME=/Operating-Platform/data/hf \
   -e DOROBOT_HOME=~/DoRobot \
-  operating-platform:V1.2
+  operating-platform:V1.3_ubuntu20
 
 # 3. 检查容器是否运行
 if [ "$(docker inspect -f '{{.State.Running}}' operating_platform 2>/dev/null)" == "true" ]; then
